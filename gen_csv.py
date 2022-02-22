9bad6f192cb9458d268bf98f76d8aa3d3c84f42a
Foris Cai <foris323@gmail.com>
2022-02-22 19:42:22 +0800

    fix bug

--- gen_csv.py
+++ gen_csv.py
@@ -3,10 +3,10 @@
 import json
 from collections import OrderedDict
 
-all_data = OrderedDict({"randread.log": "read",
-                        "randwrite.log": "write",
-                        "read.log": "read",
-                        "randwrite.log": "randwrite"})
+all_data = OrderedDict({"example_randread.json": "read",
+                        "example_randwrite.json": "write",
+                        "example_read.json": "read",
+                        "example_write.json": "write"})
 
 csv = ""
 keys = all_data.keys()
@@ -18,7 +18,7 @@ for k, v in all_data.items():
     with open(k) as f:
         data = json.load(f)
         iops = int(data["jobs"][0][v]["iops"])
-        iops_list.append(iops)
+        iops_list.append(str(iops))
 
 csv += ",".join(iops_list)
 with open("data.csv", "w") as f:
